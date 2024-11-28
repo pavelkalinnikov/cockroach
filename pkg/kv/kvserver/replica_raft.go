6f90886d71313e9b6a7aeff724f301d2d97f14fd
Pavel Kalinnikov <pavel@cockroachlabs.com>
2024-11-28 19:52:50 +0000

    kvserver: move log sync callback
    Epic: none
    Release note: none

--- pkg/kv/kvserver/replica_raft.go
+++ pkg/kv/kvserver/replica_raft.go
@@ -1755,36 +1755,6 @@ func (r *Replica) maybeCoalesceHeartbeat(
 	return true
 }
 
-// replicaSyncCallback implements the logstore.SyncCallback interface.
-type replicaSyncCallback Replica
-
-func (r *replicaSyncCallback) OnLogSync(
-	ctx context.Context, done logstore.MsgStorageAppendDone, commitStats storage.BatchCommitStats,
-) {
-	repl := (*Replica)(r)
-	// The log mark is non-empty only if this was a non-empty log append that
-	// updated the stable log mark.
-	if mark := done.Mark(); mark.Term != 0 {
-		repl.flowControlV2.SyncedLogStorage(ctx, mark)
-	}
-	// Block sending the responses back to raft, if a test needs to.
-	if fn := repl.store.TestingKnobs().TestingAfterRaftLogSync; fn != nil {
-		fn(repl.ID())
-	}
-	// Send MsgStorageAppend's responses.
-	repl.sendRaftMessages(ctx, done.Responses(), nil /* blocked */, false /* willDeliverLocal */)
-	if commitStats.TotalDuration > defaultReplicaRaftMuWarnThreshold {
-		log.Infof(repl.raftCtx, "slow non-blocking raft commit: %s", commitStats)
-	}
-}
-
-func (r *replicaSyncCallback) OnSnapSync(ctx context.Context, done logstore.MsgStorageAppendDone) {
-	repl := (*Replica)(r)
-	// NB: when storing snapshot, done always contains a non-zero log mark.
-	repl.flowControlV2.SyncedLogStorage(ctx, done.Mark())
-	repl.sendRaftMessages(ctx, done.Responses(), nil /* blocked */, true /* willDeliverLocal */)
-}
-
 // sendRaftMessages sends a slice of Raft messages.
 //
 // blocked is a set of replicas to which replication traffic is currently
--- pkg/kv/kvserver/replica_raftlog_writes.go
+++ pkg/kv/kvserver/replica_raftlog_writes.go
@@ -9,6 +9,8 @@ import (
 	"context"
 
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/logstore"
+	"github.com/cockroachdb/cockroach/pkg/storage"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 )
 
 func (r *replicaLogStorage) stateRaftMuLocked() logstore.RaftState {
@@ -31,4 +33,34 @@ func (r *replicaLogStorage) updateStateRaftMuLockedMuLocked(state logstore.RaftS
 	r.shMu.lastIndexNotDurable = state.LastIndex
 	r.shMu.lastTermNotDurable = state.LastTerm
 	r.shMu.raftLogSize = state.ByteSize
+}
+
+// replicaSyncCallback implements the logstore.SyncCallback interface.
+type replicaSyncCallback Replica
+
+func (r *replicaSyncCallback) OnLogSync(
+	ctx context.Context, done logstore.MsgStorageAppendDone, commitStats storage.BatchCommitStats,
+) {
+	repl := (*Replica)(r)
+	// The log mark is non-empty only if this was a non-empty log append that
+	// updated the stable log mark.
+	if mark := done.Mark(); mark.Term != 0 {
+		repl.flowControlV2.SyncedLogStorage(ctx, mark)
+	}
+	// Block sending the responses back to raft, if a test needs to.
+	if fn := repl.store.TestingKnobs().TestingAfterRaftLogSync; fn != nil {
+		fn(repl.ID())
+	}
+	// Send MsgStorageAppend's responses.
+	repl.sendRaftMessages(ctx, done.Responses(), nil /* blocked */, false /* willDeliverLocal */)
+	if commitStats.TotalDuration > defaultReplicaRaftMuWarnThreshold {
+		log.Infof(repl.raftCtx, "slow non-blocking raft commit: %s", commitStats)
+	}
+}
+
+func (r *replicaSyncCallback) OnSnapSync(ctx context.Context, done logstore.MsgStorageAppendDone) {
+	repl := (*Replica)(r)
+	// NB: when storing snapshot, done always contains a non-zero log mark.
+	repl.flowControlV2.SyncedLogStorage(ctx, done.Mark())
+	repl.sendRaftMessages(ctx, done.Responses(), nil /* blocked */, true /* willDeliverLocal */)
 }
